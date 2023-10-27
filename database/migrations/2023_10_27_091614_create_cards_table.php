fc657630190ed66f86672ce4008e7ede983273dd
Tanh101 <lyvantanh101@gmail.com>
2023-10-27 17:12:27 +0700

    create model and migration

---
+++ database/migrations/2023_10_27_091614_create_cards_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cards', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('word_id');
+            $table->foreign('word_id')->references('id')->on('words')->onDelete('cascade');
+            $table->unsignedBigInteger('flashcard_id');
+            $table->foreign('flashcard_id')->references('id')->on('flashcards')->onDelete('cascade');
+            $table->string('status');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cards');
+    }
+};
